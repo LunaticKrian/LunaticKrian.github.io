bbdf5ae6a1e10e6cbbf20517f368fff1d4b4878d
krian <2793260497@qq.com>
2025-08-17 22:59:27 +0800

    【Modify】评论 + 关于我 修改完成

---
+++ src/components/Comment/Comment.tsx
@@ -0,0 +1,56 @@
+import React, {JSX} from 'react'
+import {useThemeConfig, useColorMode} from '@docusaurus/theme-common'
+import Giscus, {GiscusProps} from '@giscus/react'
+import {useLocation} from '@docusaurus/router';
+
+const defaultConfig: Partial<GiscusProps> = {
+    id: 'comments',
+    mapping: 'specific',
+    reactionsEnabled: '1',
+    emitMetadata: '0',
+    inputPosition: 'top',
+    loading: 'lazy',
+    strict: '1', // 用根据路径标题自动生成的 sha1 值，精确匹配 github discussion，避免路径重叠（比如父和子路径）时评论加载串了
+    lang: 'zh-CN',
+}
+
+export default function Comment(): JSX.Element {
+    // giscus 评论功能
+    let giscusConfig = {
+        repo: 'LunaticKrian/LunaticKrian.github.io',
+        repoId: 'R_kgDOPaW76g',
+        category: 'General',
+        categoryId: 'DIC_kwDOPaW76s4CuNlp',
+    }
+
+    // merge default config
+    const giscus = {...defaultConfig, ...giscusConfig}
+
+    if (!giscus.repo || !giscus.repoId || !giscus.categoryId) {
+        throw new Error(
+            'You must provide `repo`, `repoId`, and `categoryId` to `themeConfig.giscus`.',
+        )
+    }
+
+    const path = useLocation().pathname.replace(/^\/|\/$/g, '');
+    const firstSlashIndex = path.indexOf('/');
+    var subPath: string = ""
+    if (firstSlashIndex !== -1) {
+        subPath = path.substring(firstSlashIndex + 1)
+    } else {
+        subPath = "index"
+    }
+
+    giscus.term = subPath
+    giscus.theme =
+        useColorMode().colorMode === 'dark' ? 'transparent_dark' : 'light'
+
+
+
+    return (
+        <div style={{marginTop: '50px', width: '75%'}}>
+            {/*// @ts-ignore*/}
+            <Giscus {...giscus} />
+        </div>
+    )
+}
