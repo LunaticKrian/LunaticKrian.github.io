77c0b330d7887b7424b11d055505b1b7ad2b5f4d
fanguokang_dxm <fanguokang@duxiaoman.com>
2025-08-21 18:46:00 +0800

    【Modify】默认图片替换

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -4,66 +4,66 @@ import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 
 type FeatureItem = {
-  title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
-  description: ReactNode;
+    title: string;
+    Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+    description: ReactNode;
 };
 
 // 首页展示
 const FeatureList: FeatureItem[] = [
-  {
-    title: '408备考中',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        记录408考研学习笔记中...
-      </>
-    ),
-  },
-  {
-    title: '主要还是后端',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-          励志成为全栈开发的独立游戏开发者（PS：笑死 😆）
-      </>
-    ),
-  },
-  {
-    title: '持续学习，持续成长',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        终生学习！
-      </>
-    ),
-  },
+    {
+        title: '408备考中',
+        Svg: require('@site/static/img/homepage-01.svg').default,
+        description: (
+            <>
+                记录408考研学习中...（闭关修炼内功）
+            </>
+        ),
+    },
+    {
+        title: '主要还是后端',
+        Svg: require('@site/static/img/homepage-02.svg').default,
+        description: (
+            <>
+                励志成为全栈开发的独立游戏开发者（PS：笑死 😆）
+            </>
+        ),
+    },
+    {
+        title: '持续学习，持续成长',
+        Svg: require('@site/static/img/homepage-02.svg').default,
+        description: (
+            <>
+                终生学习！看更多的风景，认识更多的人
+            </>
+        ),
+    },
 ];
 
 function Feature({title, Svg, description}: FeatureItem) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+    return (
+        <div className={clsx('col col--4')}>
+            <div className="text--center">
+                <Svg className={styles.featureSvg} role="img"/>
+            </div>
+            <div className="text--center padding-horiz--md">
+                <Heading as="h3">{title}</Heading>
+                <p>{description}</p>
+            </div>
+        </div>
+    );
 }
 
 export default function HomepageFeatures(): ReactNode {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section className={styles.features}>
+            <div className="container">
+                <div className="row">
+                    {FeatureList.map((props, idx) => (
+                        <Feature key={idx} {...props} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
